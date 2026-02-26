48327011f2ec6c079a3ea72a206549b7b49e4667
Mishhkinis <adrijusanukas@gmail.com>
2026-02-26 16:31:59 +0200

    kintamieji ir bibliotekos

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    double GBP_Bendras = 0.8729, GBP_Pirkti = 0.8600, GBP_Parduoti = 0.9220;
+    double USD_Bendras = 1.1793, USD_Pirkti = 1.1460, USD_Parduoti = 1.2340;
+    double INR_Bendras = 104.6918, INR_Pirkti = 101.3862, INR_Parduoti = 107.8546;
+    return 0;
+}
